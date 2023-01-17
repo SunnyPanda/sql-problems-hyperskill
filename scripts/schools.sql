cef54741f657982b3a7b9b09647f9651fc956e68
katja <ekaterina.kozlova@canter.fi>
2023-01-17 15:51:32 +0200

    Basic CREATE statement -> Back to school

---
+++ scripts/schools.sql
@@ -0,0 +1,7 @@
+--Create a table named schools with three columns: school_num INT, city_id INT, and founding_date DATE.
+
+CREATE TABLE schools (
+    school_num INT,
+    city_id INT,
+    founding_date DATE
+);
