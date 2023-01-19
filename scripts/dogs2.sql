f3f8f8adce2921f456b1f69c38263bef73f56531
katja <ekaterina.kozlova@canter.fi>
2023-01-19 18:23:01 +0200

    Basic INSERT statement -> Add new records

---
+++ scripts/dogs2.sql
@@ -0,0 +1,6 @@
+--Insert these three records into the table dogs with fields name (VARCHAR(20)) and dog_breed (VARCHAR(20))using one query:
+
+INSERT INTO dogs
+VALUES ('Buddy', 'Akita'),
+       ('Ruby', 'Mastiff'),
+       ('Daisy', 'Bully');
