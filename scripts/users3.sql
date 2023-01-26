254d1cb8729e16769ca4465e9391ddbb7d980c77
SunnyPanda <kashtanka.nya@gmail.com>
2023-01-26 21:21:36 +0200

    Consistency constraints -> The girl has no name

---
+++ scripts/users3.sql
@@ -0,0 +1,8 @@
+-- Create a table users with columns name VARCHAR(40), age INT, and city VARCHAR(20).
+-- The column name should be NOT NULL.
+
+CREATE TABLE users (
+    name VARCHAR(40) NOT NULL,
+    age INT,
+    city VARCHAR(20)
+);
