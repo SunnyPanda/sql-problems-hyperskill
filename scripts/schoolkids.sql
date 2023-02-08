337bff599aef096b62d0f202b3e17d56a71c13c7
SunnyPanda <kashtanka.nya@gmail.com>
2023-02-08 20:45:23 +0200

    PRIMARY KEY constraint -> No more keys

---
+++ scripts/schoolkids.sql
@@ -0,0 +1,5 @@
+-- Drop the primary key pk_schoolchild from the table schoolkids. The primary key pk_schoolchild was defined
+-- on the columns name and birthdate.
+
+ALTER TABLE schoolkids
+DROP PRIMARY KEY;
