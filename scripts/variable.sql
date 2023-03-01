67edf3e674994065e2c366b495edd6df7874d023
SunnyPanda <kashtanka.nya@gmail.com>
2023-03-01 12:43:45 +0200

    Variables in MySQL -> Cache

---
+++ scripts/variable.sql
@@ -0,0 +1,4 @@
+-- MySQL has the key_cache_block_size system variable that shows the size of blocks in the key cache in bytes.
+-- We want to know its value. Write a command that will show it.
+
+SELECT @@key_cache_block_size
